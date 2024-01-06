62a92115de7a49e1846faa15da54cd3e5c679f97
Alex Clarke <alexander15clarke@gmail.com>
2024-01-06 15:24:45 -0600

    add new admin route

--- main.go
+++ main.go
@@ -3,20 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/alex-c96/gowebdev/internal/apiconfig"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	const port = "8080"
 	const filePathRoot = "."
-	mux := http.NewServeMux()
-
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
-	corsMux := middlewareCors(mux)
+	r := chi.NewRouter()
+	apiR := chi.NewRouter()
+	adminR := chi.NewRouter()
+	apiCfg := new(apiconfig.ApiConfig)
+	fsHandler := apiCfg.MiddlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	r.Handle("/app/", fsHandler)
+	r.Handle("/app*", fsHandler)
+	apiR.Get("/healthz", handlerReadiness)
+	apiR.Get("/reset", apiCfg.Reset)
+	adminR.Get("/metrics", apiCfg.GetHits)
+	r.Mount("/admin", adminR)
+	r.Mount("/api", apiR)
+	corsr := middlewareLogger(middlewareCors(r))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
-		Handler: corsMux,
+		Handler: corsr,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
@@ -38,6 +49,13 @@ func middlewareCors(next http.Handler) http.Handler {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func middlewareLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
 }
